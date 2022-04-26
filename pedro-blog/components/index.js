528fe5f2aeb3ec42ac59f940465e74a7d0ff4634
Pedro Rosaes <pedro_rosaes@hotmail.com>
2022-04-26 00:15:17 -0300

    Creating PostCard Component and fetching data from GRAPHCMS

---
+++ pedro-blog/components/index.js
@@ -0,0 +1,7 @@
+import { default as Categories } from './Categories'
+import { default as PostCard } from './PostCard'
+import { default as PostWidget } from './PostWidget'
+import { default as Header } from './Header'
+import { default as Layout } from './Layout'
+
+export { Categories, PostCard, PostWidget, Header, Layout }
--- pedro-blog/pages/index.tsx
+++ pedro-blog/pages/index.tsx
@@ -1,28 +1,38 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const Home: NextPage = () => {
-  const posts: { title: string; excerpt: string }[] = [
-    { title: 'React Testing', excerpt: 'Learn React Testing' },
-    { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-  ]
-
+const Home = ({ posts }) => {
   return (
-    <div className="container mx-auto mb-8 bg-gray-300 px-10">
+    <div className="container mx-auto mb-8 px-10">
       <Head>
         <title>Pedro`s Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-        {posts.map((post, index) => (
-          <div key={index}>
-            {post.title}
-            {post.excerpt}
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <PostCard post={post.node} key={post.title} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
           </div>
-        ))}
+        </div>
       </div>
     </div>
   )
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: { posts },
+  }
+}
