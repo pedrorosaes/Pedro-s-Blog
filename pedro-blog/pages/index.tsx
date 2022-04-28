f8d5c91362e93b5d3f922f31e2cd706526374c02
Pedro Rosaes <pedro_rosaes@hotmail.com>
2022-04-27 21:39:43 -0300

    add: Author component

--- pedro-blog/pages/index.tsx
+++ pedro-blog/pages/index.tsx
@@ -4,36 +4,35 @@ import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
 
 const Home = ({ posts }) => {
-  console.log(posts)
-  return (
-    <div className="container mx-auto mb-8 px-10">
-      <Head>
-        <title>Pedro`s Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-        <div className="col-span-1 lg:col-span-8">
-          {posts.map((post) => (
-            <PostCard post={post.node} key={post.title} />
-          ))}
+    return (
+        <div className="container mx-auto mb-8 px-10">
+            <Head>
+                <title>Pedro`s Blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+                <div className="col-span-1 lg:col-span-8">
+                    {posts.map((post) => (
+                        <PostCard post={post.node} key={post.title} />
+                    ))}
+                </div>
+                <div className="col-span-1 lg:col-span-4">
+                    <div className="relative top-8 lg:sticky">
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-span-1 lg:col-span-4">
-          <div className="relative top-8 lg:sticky">
-            <PostWidget />
-            <Categories />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Home
 
 export const getStaticProps = async () => {
-  const posts = (await getPosts()) || []
+    const posts = (await getPosts()) || []
 
-  return {
-    props: { posts },
-  }
+    return {
+        props: { posts },
+    }
 }
--- pedro-blog/services/index.js
+++ pedro-blog/services/index.js
@@ -3,77 +3,76 @@ import { request, gql } from 'graphql-request'
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 export const getPosts = async () => {
-  const query = gql`
-    query MyQuery {
-      postsConnection {
-        edges {
-          node {
-            author {
-              bio
-              id
-              name
-              photo {
-                url
-              }
+    const query = gql`
+        query MyQuery {
+            postsConnection {
+                edges {
+                    node {
+                        author {
+                            bio
+                            id
+                            name
+                            photo {
+                                url
+                            }
+                        }
+                        createdAt
+                        slug
+                        title
+                        excerpt
+                        featuredImage {
+                            url
+                        }
+                        categories {
+                            name
+                            slug
+                        }
+                    }
+                }
             }
-            createdAt
-            slug
-            title
-            excerpt
-            featuredImage {
-              url
-            }
-            categories {
-              name
-              slug
-            }
-          }
         }
-      }
-    }
-  `
-  const results = await request(graphqlAPI, query)
+    `
+    const results = await request(graphqlAPI, query)
 
-  return results.postsConnection.edges
+    return results.postsConnection.edges
 }
 
-export const getPostsDetails = async (slug) => {
-  const query = gql`
-    query GetPostDetails($slug: String!) {
-      post(where: { slug: $slug })
-            author {
-              bio
-              id
-              name
-              photo {
-                url
-              }
+export const getPostDetails = async (slug) => {
+    const query = gql`
+        query GetPostDetails($slug: String!) {
+            post(where: { slug: $slug }) {
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                author {
+                    name
+                    bio
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                content {
+                    raw
+                }
+                categories {
+                    name
+                    slug
+                }
             }
-            createdAt
-            slug
-            title
-            excerpt
-            featuredImage {
-              url
-            }
-            categories {
-              name
-              slug
-            }
-            content {
-              raw
-            }
-          }
         }
-   
-  `
-  const results = await request(graphqlAPI, query, { slug })
+    `
 
-  return results.post
+    const result = await request(graphqlAPI, query, { slug })
+
+    return result.post
 }
 
 export const getRecentPosts = async () => {
-  const query = gql`
+    const query = gql`
   query GetPostsDetails() {
     posts (
       orderBy: createdAt_ASC
@@ -89,45 +88,45 @@ export const getRecentPosts = async () => {
   }
   `
 
-  const results = await request(graphqlAPI, query)
+    const results = await request(graphqlAPI, query)
 
-  return results.posts
+    return results.posts
 }
 
 export const getSimilarPosts = async (categories, slug) => {
-  const query = gql`
-    query GetPostDetails($slug: String!, $categories: [String!]) {
-      posts(
-        where: {
-          slug_not: $slug
-          AND: { categories_some: { slug_in: $categories } }
+    const query = gql`
+        query GetPostDetails($slug: String!, $categories: [String!]) {
+            posts(
+                where: {
+                    slug_not: $slug
+                    AND: { categories_some: { slug_in: $categories } }
+                }
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
         }
-        last: 3
-      ) {
-        title
-        featuredImage {
-          url
-        }
-        createdAt
-        slug
-      }
-    }
-  `
-  const result = await request(graphqlAPI, query, { slug, categories })
+    `
+    const result = await request(graphqlAPI, query, { categories, slug })
 
-  return result.posts
+    return result.posts
 }
 
 export const getCategories = async () => {
-  const query = gql`
-    query GetCategories {
-      categories {
-        name
-        slug
-      }
-    }
-  `
-  const result = await request(graphqlAPI, query)
+    const query = gql`
+        query GetCategories {
+            categories {
+                name
+                slug
+            }
+        }
+    `
+    const result = await request(graphqlAPI, query)
 
-  return result.categories
+    return result.categories
 }
